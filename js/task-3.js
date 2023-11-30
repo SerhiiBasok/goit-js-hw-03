71a9b17c6d9727a3f5b3a1481682ab939918d842
SerhiiBasok <sergeybsk19@gmail.com>
2023-11-30 17:04:27 +0200

    1/2/3 tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,15 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
-  const borderWidth = parseFloat(border);
-  return contentWidth + 2 * paddingWidth + 2 * borderWidth;
+function filterArray(numbers, value) {
+  let total = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      total.push(numbers[i]);
+    }
+  }
+  return total;
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
